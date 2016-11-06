4d32cbdd6604bab8175befa6fff011fa8babac15
maze88 <michael.52@msn.com>
2016-11-06 21:31:04 +0200

    PEP8-ified the code

--- backup_utility.py
+++ backup_utility.py
@@ -3,57 +3,67 @@ import os
 import shutil
 import hashlib
 
-import parse_config #module in local dir
+import parse_config # local module
+
+
+def hashhex(filepath):
+    ```Returns the hash of a file <filepath> in hex format.```
+
+    blocksize = 2**10
+    hasher = hashlib.md5()
+    with open(filepath, 'rb') as afile:
+        buf = afile.read(blocksize)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(blocksize)
+    return hasher.hexdigest()
+
+
+def index_pics(dir):
+    ```Generates a dictionary of the designated my_pic_dir,
+    using the hashhex(filepath) result as the key, and the filepath as the value.
+    ```
+
+    dir_hash = {}
+    for root, dirs, files in os.walk(unicode(dir)):
+        for file in files:
+            dir_hash[hashhex(os.path.join(root, file))] = os.path.join(root, file)
+    print('[+] File index complete!\n')
+    return dir_hash
+
 
 def main():
-    #reading configuration file to get user defined dirs.
-    dirs=config_parser.get_dirs()
-    new_things_dir=dirs['new_things_dir']
-    my_pic_dir=dirs['my_pic_dir']
-    backup_pic_dir=dirs['backup_pic_dir']
+    dirs = config_parser.get_dirs()
+    new_things_dir = dirs['new_things_dir']
+    my_pic_dir = dirs['my_pic_dir']
+    backup_pic_dir = dirs['backup_pic_dir']
 
-    #return hash of file bytes.
-    def hashhex(filepath):
-        blocksize=2**10
-        hasher=hashlib.md5()
-        with open(filepath, 'rb') as afile:
-            buf=afile.read(blocksize)
-            while len(buf)>0:
-                hasher.update(buf)
-                buf=afile.read(blocksize)
-        return hasher.hexdigest()
-
-    #generate index dictionary where hashhex(file)=key and path(file)=value. 
     print('Generating hash index of picture dir, this could take several minutes depending on its size...')
-    path_hash={}
-    for root, dirs, files in os.walk(unicode(my_pic_dir)):
-        for file in files:
-            path_hash[hashhex(os.path.join(root,file))]=os.path.join(root,file)
-    print('[+] File index complete!\n')
+    path_hash = index_pics(my_pic_dir)
 
-    #backup process:
-    print('Moving all files from: "%s" to their appropriate subdirectories under: "%s" (subdirectories\' paths based on paths from "%s")...' % (new_things_dir,backup_pic_dir,my_pic_dir))
-    report=['Done!\n\nPrinting report:']
+    print('Moving all files from: "{}" to their appropriate subdirectories under: "{}" (subdirectories\' paths based on paths from "{}")...'.format(new_things_dir, backup_pic_dir, my_pic_dir))
+    report = []  # prepared for logging results
     for file in os.listdir(unicode(new_things_dir)):
-        if hashhex(os.path.join(new_things_dir,file)) in path_hash:
-            src=os.path.join(new_things_dir,file)
-            des=os.path.join(backup_pic_dir+path_hash[hashhex(os.path.join(new_things_dir,file))][len(my_pic_dir):])
+        if hashhex(os.path.join(new_things_dir, file)) in path_hash:
+            src = os.path.join(new_things_dir, file)
+            des = os.path.join(backup_pic_dir+path_hash[hashhex(os.path.join(new_things_dir, file))][len(my_pic_dir):])
             try:
-		shutil.move(src,des)
-		report.append('[+] file backed up: %s' % file)
-	    except IOError:
-		report.append('[-] ERROR: No such directory %s.' % des)
+                shutil.move(src, des)
+                report.append('[+] file backed up: {}'.format(file))
+            except IOError:
+                report.append('[-] ERROR: No such directory {}.'.format(des))
         else:
-            report.append('[-] ERROR: Not yet placed in pic library: %s' % file)
+            report.append('[-] ERROR: Not yet placed in pic library: {}'.format(file))
 
-    #print report.
-    if len(report)>1:
-	for entry in report:
-	    print(entry)
+    if report:
+        print('Done!\n\nPrinting report:')
+        for entry in report:
+            print(entry)
     else:
-	print('[-] ERROR: No files to backup found in %s.' % new_things_dir)
+        print('[-] ERROR: No files to backup found in {}.'.format(new_things_dir))
+
     print('\nBack utility has finished running.\n')
+
 
 if __name__=='__main__':
     main()
-
--- parse_config.py
+++ parse_config.py
@@ -1,41 +1,42 @@
 import ConfigParser
 
-dir_types=['new_things_dir','my_pic_dir','backup_pic_dir']
+dir_types = ['new_things_dir', 'my_pic_dir', 'backup_pic_dir']
 
 def get_dirs():
-    config=ConfigParser.SafeConfigParser()
+    config = ConfigParser.SafeConfigParser()
     config.read('dirs.cfg')
-    dirs={}
+    dirs = {}
     for dir in dir_types:
-        dirs[dir]=config.get('user',dir)
-
+        dirs[dir] = config.get('user', dir)
     return dirs
 
+
 def set_dirs():
-    config=ConfigParser.RawConfigParser()
+    config = ConfigParser.RawConfigParser()
     config.read('dirs.cfg')
     for dir in dir_types:
         try:
-	    dirs[dir]=config.get('user',dir)
-	except:
-	    pass
-    print('Configuring directories...\n\'my_pic_dir\' - the dir you store your pictures in.')
-    print('\'backup_pic_dir\' - the dir you store your pictures\' backups in.')
-    print('\'new_things_dir\' - the dir you temporarily store your new pictures in.\n')
-    for key in [key for key,value in dirs.items() if value=='']:
-	config.set('user',key,raw_input('Please enter full path of direcory for \'{}\' value: '.format(key)))
-    with open('dirs.cfg','wb') as cfg_file:
-	config.write(cfg_file)
+            dirs[dir] = config.get('user', dir)
+        except:
+            pass
+    print('Configuring directories...\n"my_pic_dir" - the dir you store your pictures in.')
+    print('"backup_pic_dir" - the dir you store your pictures\' backups in.')
+    print('"new_things_dir" - the dir you temporarily store your new pictures in.\n')
+    for blank_key in [key for key, value in dirs.items() if value == '']:
+        config.set('user', blank_key, raw_input('Please enter full path of direcory for "{}" value: '.format(blank_key)))
+    with open('dirs.cfg', 'wb') as cfg_file:
+        config.write(cfg_file)
+
 
 def main():
     if '' in get_dirs().values():
-	set_dirs()
-	main()
+        set_dirs()
+        main()
     else:
         print('Current directories configured:')
         for key, value in get_dirs().items():
-            print('[+] {}: "{}"'.format(key,value))
+            print('[+] {}: "{}"'.format(key, value))
+
 
 if __name__=='__main__':
     main()
-
