971950199ef056cfffc0c40a2a45f8804f4fa950
maze88 <michael.52@msn.com>
2017-09-18 19:33:29 +0300

    Removed config parsing function.

--- backup_utility2.py
+++ backup_utility2.py
@@ -1,3 +1,4 @@
+import filecmp
 import os
 import shutil
 
@@ -13,51 +14,42 @@ def filePathsList(rootDir):
     return filePathsList
 
 def main():
-    # temp dir config for testing
+    print('\nRunning backup utility...')
+
+    # set directories here for backup utility to use:
     originDir = '/home/maze/Documents/programming/my-backup-utility/testPicDir'
     backupDir = '/home/maze/Documents/programming/my-backup-utility/testBackupDir'
+
+    print('\tOrigin directory: {}\n\tBackup directory: {}\n'.format(originDir, backupDir))
 
     # create lists of origin and backup dirs.
     originListing = filePathsList(originDir)
     backupListing = filePathsList(backupDir)
 
-    print('\nRunning backup utility...')
-    print('\tOrigin directory: {}\n\tOrigin directory: {}\n'.format(originDir, backupDir))
     log = []
     for filePath in originListing:
-        subPath = filePath[len(originDir) + 1:]  # sub path to file within originDir.
+        subPath = filePath[len(originDir) + 1:]        # sub path to file within originDir.
         backupPath = os.path.join(backupDir, subPath)  # destination path in backupDir.
-        if backupPath in backupListing:  # filename already exists in backup.
-            if os.path.samefile(filePath, backupPath):  # file already backed up.
+        if backupPath in backupListing:                # filename already exists in backup.
+            if filecmp.cmp(filePath, backupPath):      # backup file already exists.
                 pass
-            else:  # different versions in origin & backup dirs.
-                log.append('[-] version conflict - file with same name already exists in backup:' + subPath)
-        else:  # file path doesn't yet exist.
-            if os.path.exists(os.path.dirname(backupPath)):  # destination dir exists
+            else:                                      # different versions in origin & backup dirs.
+                log.append('[-] file with same name already exists in backup:' + subPath)
+        else:                                          # file path doesn't yet exist.
+            if os.path.exists(os.path.dirname(backupPath)):  # destination dir exists.
                 pass
-            else:  # destination dir doesn't exist.
+            else:                                      # destination dir doesn't exist.
                 subDir = os.path.dirname(subPath)
-                os.mkdir(os.path.join(backupDir, subDir))
+                os.mkdir(os.path.join(backupDir, subDir))  # create destination dir in backup.
                 log.append('[+] directory created in backup: ' + subDir)
-            shutil.copy(filePath, backupPath)
-            log.append('[+] successfully backed up: ' + subPath)
+            shutil.copy(filePath, backupPath)          # copy origin file to backup.
+            log.append('[+] backed up: ' + subPath)
 
-    print('Backup utility has finished running. Printing log...')
+    print('Backup utility has finished running. Printing log:')
     log.sort()
     for entry in log:
-        print(entry)
+        print('\t' + entry)
     print('\nExiting...')
 
 if __name__ == '__main__':
     main()
-
-
-"""
-# environment setup
-if configFile:
-    configedDirs = read(configFile)
-if configedDirs:
-    print(configedDirs)
-if input:
-    configedDirs = input
-"""
