940ab7b9bacd5d5acbd464e5588fbe01d6fda03e
maze88 <michael.52@msn.com>
2017-10-01 10:42:14 +0300

    Refactoring and tested.

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # my-backup-utility
 The script is designed to save work managing the many sub-directories I store my backups in.
 
-The program requires two input dirs to work:
+The program requires two input dirs to work, which are manually set in 'dirs.cfg':
 
 1. 'originDir' - the source dir to backup.
 2. 'backupDir' - the dir in which the backup is stored.
 
 To go into a bit more detail:
-The sub-dirs of 'originDir' and 'backupDir' are indexed, then compared.
-Files with same name but different content are logged.
-Files with same name and content are, obviously, skipped.
-Missing sub-directories are created in 'backupDir', reflecting 'originDir'.
-Files that do not have a copy under 'backupDir' are copied to their appropriate sub-directories in 'backupDir'.
+The sub-paths of 'originDir' and 'backupDir' are indexed and then compared;
+
+* Files that do not have a copy under 'backupDir' are copied to their appropriate sub-directories in 'backupDir'.
+* Files with same name and content are skipped, as they clearly exist already in backup.
+* Files with same name but different content will be logged.
+* Missing and required (non-empty) sub-directories are created in 'backupDir', to reflect 'originDir'.
+
+The folder 'initTestState' contains two folders (an origin and backup) which can be used for testing, after configuring their paths in 'dirs.cfg'.
 
--- my_backup_utility.py
+++ my_backup_utility.py
@@ -1,5 +1,6 @@
 import filecmp
 import os
+import re
 import shutil
 
 def filePathsList(rootDir):
@@ -13,19 +14,22 @@ def filePathsList(rootDir):
             filePathsList.append(fullPath)
     return filePathsList
 
-def setDirs():  # incomplete docstring & functionality
+def getSetting(settingsFile, settingTitle):
     """
-    Set origin and backup dirs here for utility to use:
+    Reads 'settingsFile' and finds the last instance of 'settingTitle'. Returns the
+    value of 'settingTitle' that comes after te '=' sign, removing any trailing whitespaces
+    and any closing '/' or '\'.
+    If not found, returns False.
     """
-    originDir = '/home/maze/Pictures'
-    backupDir = '/media/maze/Samsung USB/Pictures'
+    setting = False
+    with open(settingsFile, 'r') as settings:
+        for line in settings:
+            settingRegexp = re.search(settingTitle + '\s*=\s*(.+)', line)
+            if settingRegexp:
+                setting = settingRegexp.group(1).strip().rstrip('/').rstrip('\\')
 
-    if not os.path.exists(originDir) or not os.path.exists(backupDir):
-        print('[-] ' + 'Specifified origin and/or backup directory do not exist.\nExiting...')
-        exit()
-    else:
-        print('\tOrigin directory: {}\n\tBackup directory: {}\n'.format(originDir, backupDir))
-        return (originDir, backupDir)
+    re.purge()
+    return setting
 
 def printSortedLines(array, precursor=''):
     """
@@ -44,28 +48,36 @@ def printSortedLines(array, precursor=''):
 def main():
     print('Running backup utility...\n')
 
+    # initiation block
     goodIcon = '[+] '
     badIcon = '[-] '
     eventsLog = []
-    originDir, backupDir = setDirs()
+    originDir = getSetting('dirs.cfg', 'originDir')
+    backupDir = getSetting('dirs.cfg', 'backupDir')
+    print('\tOrigin directory: {}\n\tBackup directory: {}\n'.format(originDir, backupDir))
+    if not os.path.exists(originDir) or not os.path.exists(backupDir):
+        print('[-] ' + 'Specifified origin and/or backup directory do not exist.\nExiting...')
+        exit()
     originListing = filePathsList(originDir)
     backupListing = filePathsList(backupDir)
 
     # main logic block
     for filePath in originListing:
-        subPath = filePath[len(originDir) + 1:]  # sub path to file within originDir.
+        subPath = filePath[len(originDir) + 1:]  # sub-dir path to file
+        subDir = os.path.dirname(subPath)
         backupPath = os.path.join(backupDir, subPath)
-        if backupPath in backupListing and not filecmp.cmp(filePath, backupPath):
-            eventsLog.append(badIcon + 'File with same name already exists in backup:' + subPath)
+        if backupPath in backupListing:
+            sameFile = filecmp.cmp(filePath, backupPath, False)
+            if not sameFile:
+                eventsLog.append(badIcon + 'Different file with same name already exists in backup:' + subPath)
         else:
-            if not os.path.exists(os.path.dirname(backupPath)):
-                subDir = os.path.dirname(subPath)
+            if not os.path.exists(os.path.join(backupDir, subDir)):
                 os.mkdir(os.path.join(backupDir, subDir))
                 eventsLog.append(goodIcon + 'Directory created in backup: ' + subDir)
             shutil.copy(filePath, backupPath)
             eventsLog.append(goodIcon + 'Backed up: ' + subPath)
 
-    # finishing up
+    # termination block
     print('Backup utility has finished running. Printing log:\n')
     if not printSortedLines(eventsLog, '\t'):
         print('\t' + goodIcon + 'All file backups up to date.')
